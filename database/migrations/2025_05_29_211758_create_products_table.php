49b26050ae0fb4056c1dbc6fd2853190a73f5b95
Felipe Ramos <felipe.ramos@idx.digital>
2025-05-29 18:34:30 -0300

    Integrar migrations

---
+++ database/migrations/2025_05_29_211758_create_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id')->constrained()->cascadeOnDelete();
+            $table->string('name', 150);
+            $table->string('price');
+            $table->integer('current_stock');
+            $table->boolean('is_active')->default(true);
+            $table->timestamps();
+
+            $table->index('category_id');
+            $table->index('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
